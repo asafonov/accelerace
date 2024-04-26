7a2f80405fc49eb48e2811a46f82c817c069a13f
asafonov <me@asafonov.org>
2024-04-26 11:09:44 +0000

    add moveTo method to control via gyroscope

--- view/car.js
+++ view/car.js
@@ -9,7 +9,7 @@ class CarView {
       height: rect.height + rect.top,
       width: rect.width + rect.left,
       left: this.roadRect.left + this.roadRect.width / 2 - rect.width / 2,
-      top: window.innerHeight - rect.height * 1.5 - rect.top
+      top: window.innerHeight - rect.height * 2 - rect.top
     }
     this.speed = this.roadRect.width / (speed || asafonov.timer.getFPS())
     asafonov.player = this.carRect
@@ -20,11 +20,13 @@ class CarView {
   addEventListeners() {
     asafonov.messageBus.subscribe(asafonov.events.CAR_MOVE_RIGHT, this, 'onMoveRight')
     asafonov.messageBus.subscribe(asafonov.events.CAR_MOVE_LEFT, this, 'onMoveLeft')
+    asafonov.messageBus.subscribe(asafonov.events.CAR_MOVE_TO, this, 'onMoveTo')
   }
 
   removeEventListeners() {
     asafonov.messageBus.unsubscribe(asafonov.events.CAR_MOVE_RIGHT, this, 'onMoveRight')
     asafonov.messageBus.unsubscribe(asafonov.events.CAR_MOVE_LEFT, this, 'onMoveLeft')
+    asafonov.messageBus.unsubscribe(asafonov.events.CAR_MOVE_TO, this, 'onMoveTo')
   }
 
   display() {
@@ -38,6 +40,33 @@ class CarView {
 
   onMoveLeft() {
     if (! this.isMoving) this.moveLeft()
+  }
+
+  onMoveTo (to) {
+    this.stop()
+    to = Math.min(to, 1)
+    to = Math.max(to, -1)
+    const middle = this.roadRect.left + this.roadRect.width / 2 - this.carRect.width / 2
+    to = middle + this.roadRect.width / 2 * to
+    this.carRect.left = to
+    this.correctRoadBoundaries()
+    this.display()
+  }
+
+  correctRoadBoundaries() {
+    let corrected = false
+
+    if (this.carRect.left <= this.roadRect.left) {
+      this.carRect.left = this.roadRect.left
+      corrected = true
+    }
+
+    if (this.carRect.left >= this.roadRect.right - this.carRect.width) {
+      this.carRect.left = this.roadRect.right - this.carRect.width
+      corrected = true
+    }
+
+    return corrected
   }
 
   move(top, left) {
@@ -55,15 +84,7 @@ class CarView {
         movedHorizontally = false
       }
 
-      if (this.carRect.left <= this.roadRect.left) {
-        this.carRect.left = this.roadRect.left
-        movedHorizontally = false
-      }
-
-      if (this.carRect.left >= this.roadRect.right - this.carRect.width) {
-        this.carRect.left = this.roadRect.right - this.carRect.width
-        movedHorizontally = false
-      }
+      movedHorizontally = movedHorizontally && ! this.correctRoadBoundaries()
     }
 
     this.display()
@@ -81,6 +102,7 @@ class CarView {
   }
 
   stop() {
+    this.isMoving = false
     this.timeout && asafonov.timer.remove(this.timeout)
     this.timeout = null
   }
